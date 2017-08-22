04faf81e1a6e5c2796222b10c7fcb4cf8bbddfa2
Fernando Ferreira <fferreira12@gmail.com>
2017-08-22 17:53:56 -0300

    Fixes model.ts file content

--- app/model.js
+++ app/model.js
@@ -1,8 +1,21 @@
 "use strict";
-exports.model = {
-    user: "Adam",
-    items: [{ action: "Buy Flowers", done: false },
-        { action: "Get Shoes", done: false },
-        { action: "Collect Tickets", done: true },
-        { action: "Call Joe", done: false }]
-};
+var Model = (function () {
+    function Model() {
+        this.user = "Adam";
+        this.items = [
+            new TodoItem("Buy Flowers", false),
+            new TodoItem("Get Shoes", false),
+            new TodoItem("Collect Tickets", false),
+            new TodoItem("Call Joe", false)];
+    }
+    return Model;
+}());
+exports.Model = Model;
+var TodoItem = (function () {
+    function TodoItem(action, done) {
+        this.action = action;
+        this.done = done;
+    }
+    return TodoItem;
+}());
+exports.TodoItem = TodoItem;
